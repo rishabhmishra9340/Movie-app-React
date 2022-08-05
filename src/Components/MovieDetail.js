b85f635b4ab2c065a59c2f7cf9b66b01faaa5f3b
rishabhmishra9340 <104819756+rishabhmishra9340@users.noreply.github.com>
2022-08-05 19:39:14 +0530

    seco

--- src/Components/MovieDetail.js
+++ src/Components/MovieDetail.js
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from "react"
 import { useParams } from "react-router-dom"
 import "./movie.css"
+import{Link}from "react-router-dom"
+
+
 const MovieDetail = () => {
     const [currentMovieDetail, setMovie] = useState()
     const { id } = useParams()
@@ -15,15 +18,21 @@ const MovieDetail = () => {
         .then(res => res.json())
         .then(data => setMovie(data))
     }
+    
 
     return (
+
+        
         <div className="container">
+            
             <div className="detail">
+            <div className="back-to-home">
+                <Link to='/'>  <i class="fa fa-long-arrow-left" aria-hidden="true" ></i></Link> </div>
             <h1>{currentMovieDetail.original_title }</h1>
-               <h4>Rating : {(currentMovieDetail.vote_average/2).toFixed(1)}/5</h4>
+               <h4>Rating : {(currentMovieDetail.vote_average/2).toFixed(2)}/5</h4>
                   <p>{currentMovieDetail.overview}</p>
-                  <h4>Release Date - {currentMovieDetail.release_date}</h4>
-                  <h4>Original-language - {currentMovieDetail.original_language}</h4>
+                  <h4><span>Release Date</span>  {currentMovieDetail.release_date}</h4>
+                  <h4><span>Original Language</span>  {currentMovieDetail.original_language}</h4>
             </div>
             <div className="poster">
                 <img src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`}></img>
