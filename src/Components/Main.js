25a855e2b3b70e762db47cfc1cf6ea1f04ba5333
rishabhmishra9340 <104819756+rishabhmishra9340@users.noreply.github.com>
2022-08-09 13:38:26 +0530

    sec

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,10 +2,11 @@
 import React,{useState} from "react";
 import { useEffect } from "react";
 import Card from "./Card";
-let API_key="&api_key=db95773a7fb212ba790d71f6adac0e7e";
+let API_key="&api_key=461182fa2668493a72758c55a1789c35";
 let base_url="https://api.themoviedb.org/3";
 let url=base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
 console.log(url)
+// https://api.themoviedb.org/3/movie/550?api_key=461182fa2668493a72758c55a1789c35
 const Main=()=>{
     const [movieData,setData]=useState([]);
     const [url_set,setUrl]=useState(url);
--- src/Components/MovieDetail.js
+++ src/Components/MovieDetail.js
@@ -14,7 +14,7 @@ const MovieDetail = () => {
     }, [])
 
     const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=461182fa2668493a72758c55a1789c35&language=en-US`)
         .then(res => res.json())
         .then(data => setMovie(data))
     }
@@ -24,21 +24,41 @@ const MovieDetail = () => {
 
         
         <div className="container">
-            
+          
             <div className="detail">
-            <div className="back-to-home">
-                <Link to='/'>  <i class="fa fa-long-arrow-left" aria-hidden="true" ></i></Link> </div>
-            <h1>{currentMovieDetail.original_title }</h1>
-               <h4>Rating : {(currentMovieDetail.vote_average/2).toFixed(2)}/5</h4>
-                  <p>{currentMovieDetail.overview}</p>
-                  <h4><span>Release Date</span>  {currentMovieDetail.release_date}</h4>
-                  <h4><span>Original Language</span>  {currentMovieDetail.original_language}</h4>
+              <div className="movie__name">{currentMovieDetail ? currentMovieDetail.original_title : ""}</div>
+              <div className="movie__rating">
+                            {currentMovieDetail ? currentMovieDetail.vote_average/2: ""}/5 <i class="fas fa-star" />
+                        </div>
+                        <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
+                        <div className="movie__releaseDate">{currentMovieDetail ? "Release date: " + currentMovieDetail.release_date : ""}
+                        <h4>Original Language  {currentMovieDetail ?currentMovieDetail.original_language:""}</h4>
+                        
             </div>
-            <div className="poster">
-                <img src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`}></img>
-            </div>
+            
         </div>
+        <div className="poster">
+                <img className="img" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`} />
+            </div>
+            {/* <div className="container"> */}
+{/*             
+<div className="detail">
+<div className="back-to-home">
+    <Link to='/'>  <i class="fa fa-long-arrow-left" aria-hidden="true" ></i></Link> </div>
+<h1>{currentMovieDetail.original_title }</h1>
+   <h4>Rating : {(currentMovieDetail.vote_average/2).toFixed(2)}/5</h4>
+      <p>{currentMovieDetail.overview}</p>
+      <h4><span>Release Date</span>  {currentMovieDetail.release_date}</h4>
+      <h4><span>Original Language</span>  {currentMovieDetail.original_language}</h4>
+</div>
+<div className="poster">
+    <img src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`}></img>
+</div> */}
+</div>
+        // </div>
+        
     )
+}
 
-    }
+      
 export default MovieDetail
